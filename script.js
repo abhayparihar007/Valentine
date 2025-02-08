6b7f97d1fdd7b23bc248a88779fa1e0a7ba5ec01
abhayparihar007 <135969917+abhayparihar007@users.noreply.github.com>
2025-02-09 02:39:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const resultDiv = document.getElementById('result');
 
 const secretCode = 'yashika'; // Replace with your secret code
 const secretCode2 = 'karishma'; 
+const secretCode3 = 'kashish'; 
+const secretCode4 = 'mansi'; 
+const secretCode5 = 'disha'; 
 const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 
 form.addEventListener('submit', (e) => {
@@ -14,6 +17,15 @@ form.addEventListener('submit', (e) => {
 		resultDiv.innerHTML = 'Baby';
 		window.location.href = nextPageUrl; // Redirect to new page
 	}else if (userInput === secretCode2) {
+		resultDiv.innerHTML = 'Baby';
+		window.location.href = nextPageUrl; // Redirect to new page
+	}else if (userInput === secretCode3) {
+		resultDiv.innerHTML = 'Baby';
+		window.location.href = nextPageUrl; // Redirect to new page
+	}else if (userInput === secretCode4) {
+		resultDiv.innerHTML = 'Baby';
+		window.location.href = nextPageUrl; // Redirect to new page
+	} else if (userInput === secretCode5) {
 		resultDiv.innerHTML = 'Baby';
 		window.location.href = nextPageUrl; // Redirect to new page
 	} 
